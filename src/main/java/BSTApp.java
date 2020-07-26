96d492ef80874af5177a26d9e5f5231ac6017f41
Andrew-Rose1 <55816533+Andrew-Rose1@users.noreply.github.com>
2020-07-26 13:29:49 -0700

    Add files via upload

--- src/main/java/BSTApp.java
+++ src/main/java/BSTApp.java
@@ -15,9 +15,13 @@ public class BSTApp{
     int size = 10;
     BST obj4 = new BST();
     int[] randomArr = new int[size];
-		for (int i = 0; i < size; i++) {
-			randomArr[i] = (int)(Math.random()*100);
-		}
+    for (int i = 0; i < size; i++) {
+			int tryVal = (int)(Math.random()*1000);
+			while (!checkArr(randomArr, tryVal)) {
+				tryVal = (int)(Math.random()*1000);
+			}
+			randomArr[i] = tryVal;
+    }
     for (int i = 0; i < randomArr.length; i++) {
 				obj4.insert(randomArr[i]);
 				System.out.println("Insert " + randomArr[i]);
@@ -33,22 +37,22 @@ public class BSTApp{
     for (int i = 0; i < data.size()-1; i++) {
       minimumDiff = data.get(i+1) - data.get(i);
       intArray[i] = minimumDiff;
-      // System.out.println("Diff of index " + data.get(i) + " and " + data.get(i+1) + " is " + minimumDiff);
+       System.out.println("Diff of index " + data.get(i) + " and " + data.get(i+1) + " is " + minimumDiff);
     }
     //TESTING// Print out the array holding all of the differences.
-    //for (int j = 0; j < intArray.length; j++) {
-      //System.out.println(intArray[j]);
-    //}
+    for (int j = 0; j < intArray.length; j++) {
+      System.out.println(intArray[j]);
+    }
 
     // Set minimum difference to the first difference value, and compare all differences
     minimumDiff = intArray[0];
     for (int m = 1; m < intArray.length; m++) {
       // If the index we are checking has a value smaller than the saved minimumDiff, then assign it to minimumDiff
       if (minimumDiff > intArray[m]) {
-        //TESTING//  System.out.println("Spot " + m + " is a smaller diff than " + minimumDiff);
+          System.out.println("Spot " + m + " is a smaller diff than " + minimumDiff);
         minimumDiff = intArray[m];
       }
-      // System.out.println("Current minDiff is " + minimumDiff);
+       System.out.println("Current minDiff is " + minimumDiff);
     }
     return minimumDiff;
   }
@@ -60,4 +64,14 @@ public class BSTApp{
           inOrderTraversal(node.right);
       }
   }
+
+
+  public static boolean checkArr(int [] arr, int num) {
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] == num) {
+				return false;
+			}
+		}
+		return true;
+	}
 }
